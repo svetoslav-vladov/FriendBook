ed8dd5c8e798bf51d16c02a4c876c941fc1f8dac
Svetoslav Vladov <svetoslavladov@gmail.com>
2018-03-17 12:11:26 +0200

    REMOVED: #wrap div from header and footer, put the #wrap in login and register manualy, MAIN page css REworked, it clones facebook: reason is that fixed position do not work well with grid, still we use GRID CSS. More CSS tweaks are coming!!!

--- main.php
+++ main.php
@@ -3,21 +3,8 @@
 ?>
 
 
-
-<div id="gridWrap">
-    <div id="left-col-grid">
-        <div id="profile-pic"></div>
-        <div id="user-nav">
-            <ul>
-                <li><a href="#">News Feed</a></li>
-                <li><a href="#">Profile page</a></li>
-                <li><a href="#">Messages</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">friends</a></li>
-            </ul>
-        </div>
-    </div>
-    <div id="top-grid">
+<div id="top">
+    <div class="center-content">
         <div id="search">
             <a href="./index.php"><img src="#" alt="friendbook logo"></a>
             <input type="text" name="search" placeholder="Search for someone...">
@@ -31,6 +18,22 @@
                 <li><a href="index.php?page=logout"><img src="#" alt="logout"></a></li>
             </ul>
         </nav>
+    </div>
+</div>
+<div id="gridWrap">
+    <div id="left-col-grid">
+        <div class="lwrap">
+            <div id="profile-pic">asd</div>
+            <div id="user-nav">
+                <ul>
+                    <li><a href="#">News Feed</a></li>
+                    <li><a href="#">Profile page</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">friends</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
     <div id="content-grid">cen</div>
     <div id="right-col-grid">right</div>
