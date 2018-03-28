d06b4e0220b61a260c6153fa710670649858b0a0
Eray Myumyun <eraymmn@abv.bg>
2018-03-28 15:56:48 +0300

    Added new function for own posts of users, the function return info for owner of posts

--- V2.0/model/posts_dao.php
+++ V2.0/model/posts_dao.php
@@ -18,3 +18,16 @@ function getAllPosts() {
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
+
+function getOwnPosts($user_id) {
+    // This function return information for all posts and information for the owner of the posts
+    $pdo = $GLOBALS['pdo'];
+    $statement = $pdo->prepare("SELECT posts.id AS post_id, posts.description, posts.create_date, users.id AS user_id, users.first_name, users.last_name, users.profile_pic, users.profile_cover
+                                FROM posts
+                                JOIN users ON posts.user_id = users.id 
+                                WHERE users.id = ? 
+                                ORDER BY posts.create_date DESC;");
+    $statement->execute(array($user_id));
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
--- V2.0/view/profile.php
+++ V2.0/view/profile.php
@@ -1,4 +1,9 @@
 <?php
+    require_once '../include/session.php';
+    require_once "../model/posts_dao.php";
+
+    echo '<pre>';
+    var_dump(getOwnPosts($_GET['id']));
+    echo '</pre>';
 ?>
 
-profile page of user
