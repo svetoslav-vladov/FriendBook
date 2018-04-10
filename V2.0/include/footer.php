aafa55f258e9959ed3561f9ce701f53e7f678d73
Eray Myumyun <eraymmn@abv.bg>
2018-04-10 21:26:02 +0300

    some changes on footer.php for skip error and success message

--- V2.0/include/footer.php
+++ V2.0/include/footer.php
@@ -7,6 +7,21 @@
         <script src="../assets/js/send_request_friend.js"></script>
         <script src="../assets/js/profile_nav.js"></script>
         <script src="../assets/js/main.js"></script>
-
+        <script>
+            $(document).ready(function () {
+                $('.err_msg').click(function () {
+                    $(this).fadeOut();
+                });
+                $('.ok_msg').click(function () {
+                    $(this).fadeOut();
+                });
+                $('#ajax_error').click(function () {
+                    $(this).fadeOut();
+                });
+                $('#ajax_success').click(function () {
+                    $(this).fadeOut();
+                });
+            });
+        </script>
     </body>
 </html>
